9d2f17bc790526c4b3ac19c2d135a65fdccba3ad
Ferdinand Keller <ferdinandklr@gmail.com>
2022-11-16 08:27:21 +0000

    📝 updated crate's documentation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+//! This algorithm will compute the length of the Collatz sequence for all numbers in a given range.
+//! It will then output:
+//!     - The number in the provided range with the longest Collatz sequence.
+//!     - The biggest number computed while computing the Collatz sequence.
+//! 
+//! # Collatz Sequence
+//! The Collatz sequence is defined as follows:
+//!     - If the number is even, divide it by 2.
+//!     - If the number is odd, multiply it by 3 and add 1.
+//! And you repeat this process until you reach 1. The length of the sequence is the number of steps needed to reach 1.
+//! 
+//! The Collatz conjecture is the conjecture that for any number, the Collatz sequence will always reach 1.
+//! Hypothetically, the sequence could go on forever, or find another cycle, as this conjecture has not yet been proven.
+
 use std::{collections::HashMap, str::FromStr};
 use num_bigint::BigUint;
 use num_format::{Locale, ToFormattedString};
 
-/// This algorithm will compute the length of the Collatz sequence for all numbers in a given range.
-/// It will then output:
-///     - The number in the provided range with the longest Collatz sequence.
-///     - The biggest number computed while computing the Collatz sequence.
-/// 
-/// # Collatz Sequence
-/// The Collatz sequence is defined as follows:
-///     - If the number is even, divide it by 2.
-///     - If the number is odd, multiply it by 3 and add 1.
-/// And you repeat this process until you reach 1. The length of the sequence is the number of steps needed to reach 1.
-/// 
-/// The Collatz conjecture is the conjecture that for any number, the Collatz sequence will always reach 1.
-/// Hypothetically, the sequence could go on forever, or find another cycle, as this conjecture has not yet been proven.
 fn main() {
     // create a new HashMap that will cache the length of each Collatz sequence
     // this way if a sequence ends up somewhere we know, we don't have to recompute everything
