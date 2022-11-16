2c5b6bb1b67ace1b560a43774bb3d32001e7d543
Ferdinand Keller <ferdinandklr@gmail.com>
2022-11-16 08:45:52 +0000

    📝 corrected list indentation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 //! This algorithm will compute the length of the Collatz sequence for all numbers in a given range.
 //! It will then output:
-//!     - The number in the provided range with the longest Collatz sequence.
-//!     - The biggest number computed while computing the Collatz sequence.
+//! - The number in the provided range with the longest Collatz sequence.
+//! - The biggest number computed while computing the Collatz sequence.
 //! 
 //! # Collatz Sequence
 //! The Collatz sequence is defined as follows:
-//!     - If the number is even, divide it by 2.
-//!     - If the number is odd, multiply it by 3 and add 1.
+//! - If the number is even, divide it by 2.
+//! - If the number is odd, multiply it by 3 and add 1.
 //! And you repeat this process until you reach 1. The length of the sequence is the number of steps needed to reach 1.
 //! 
 //! The Collatz conjecture is the conjecture that for any number, the Collatz sequence will always reach 1.
